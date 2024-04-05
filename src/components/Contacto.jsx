5bb32199ad909ce7c3578b86e009a1df63a0e4bc
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-04-05 00:01:19 -0300

    debugging console log eliminated!

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -10,7 +10,7 @@ export const Contacto = () => {
     const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
     const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID
     const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY    
-    console.log(SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY)
+    
     e.preventDefault();      
     emailjs
       .sendForm(`${SERVICE_ID}`, `${TEMPLATE_ID}`, form.current, {
