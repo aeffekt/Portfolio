770f7ecd0f372ccd80cd2f8eab165f4041dde564
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-04-05 16:38:50 -0300

    header animation changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,12 @@ function Header() {
     <div className='new-block'  id='home'>
       <section className='header-container'>
           <img loading='lazy' decoding='async' className="rounded-circle" src={ logoAA } alt="Imagen" />
-          <div className='text-container'>
-            <div>
-              <h1 className='distint-color'><strong>Agustín Arnaiz</strong></h1>   
-              <p>Systems Analyst and <strong className='distint-color'>Full Stack Software Developer</strong>.</p>
-              <p>
-                <i>"I am highly driven by the process of <strong>creating new things.</strong></i>"
-              </p>
-            </div>
+          <div className='text-container'>            
+            <h1 className='distint-color'><strong>Agustín Arnaiz</strong></h1>   
+            <p>Systems Analyst and <strong className='distint-color'>Full Stack Software Developer</strong>.</p>
+            <p>
+              <i>"I am highly driven by the process of <strong>creating new things.</strong></i>"
+            </p>            
             <Redes />
           </div>        
       </section>          
