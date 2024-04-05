9336f0d16e3f8bd85a3fedc3f99af61461fa66bc
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-04-05 16:17:30 -0300

    reCAPTCHA security send form

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Agustín Arnaiz React Portfolio
 
-visit: arnaizagustin.vercel.app
+visit: arnaizagustin.vercel.app or arnaiz.com.ar
 
 To run locally:
 download repository
 create .env file with your 3 keys for emailjs: VITE_SERVICE_ID, VITE_TEMPLATE_ID, VITE_PUBLIC_KEY
-and run: > npm run dev
+run: > npm run dev
 
 packages:
 @mailjs/browser (contact email)
+react-google-recaptcha
 sonner (toast)
 tech-stack-icons
+
+
+Para usar emailjs, crear cuenta, crear template, registrarse en google reCaptha, registrar dominio (localhost), usar V2. import ReCaptcha y setear: sitekey="YOUR_WEB_KEY". En settings del template, reCaptcha_secret_key="YOUR_SECRET_KEY"
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,20 +1,30 @@
 import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser'
+import ReCAPTCHA from "react-google-recaptcha";
 import '../styles/Contacto.css'
 
 import { Toaster, toast } from 'sonner'
 
 
 export const Contacto = () => {  
-  
+  // Variables de entorno
+  const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
+  const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID 
+  const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY 
+  const RECAPTCHA_WEB_SITE = import.meta.env.VITE_RECAPTCHA_WEB_SITE    
+
   const form = useRef();
   function sendEmail(e) {
-    toast.info("Sending...")
-    const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
-    const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID
-    const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY    
-    
-    e.preventDefault();      
+    e.preventDefault();
+
+    // Verificar si el valor del captcha es nulo
+    const captchaValue = form.current['g-recaptcha-response'].value;
+    if (!captchaValue) {
+      toast.warning("Please complete the reCAPTCHA.");
+      return;
+    }
+
+    toast.info("Sending...") 
     emailjs
       .sendForm(`${SERVICE_ID}`, `${TEMPLATE_ID}`, form.current, {
         publicKey: `${PUBLIC_KEY}`,
@@ -24,7 +34,7 @@ export const Contacto = () => {
           toast.success('Message sent!')          
         },
         (error) => {          
-          toast.error("Failed to send the message, try again!")
+          toast.error("Failed to send the message, mark the reCaptcha and try again!")
           console.log(error.text);
         },
       );
@@ -38,14 +48,19 @@ export const Contacto = () => {
           <label>Name</label>
           <input type="text" name="user_name" />
           <label>Email</label>
-          <input type="email" name="user_email" />
+          <input type="email" name="user_email" required />
           <label>Message</label>
-          <textarea name="message" />
+          <textarea name="message" required />           
           <input type="submit" value="Send" />
+          <ReCAPTCHA
+            className='recaptcha'            
+            name="captcha"
+            sitekey={RECAPTCHA_WEB_SITE}            
+            required
+          />
         </form>
       </section>
-    </div>
-    
+    </div>    
   )
 }
 
