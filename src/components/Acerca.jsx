61899845237f1b15732291bcbd445d416b817c1b
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-04-06 01:24:57 -0300

    visual's upgrades at projects block

--- src/components/Acerca.jsx
+++ src/components/Acerca.jsx
@@ -7,21 +7,20 @@ function Acerca() {
       <section className='acerca-container'>      
         <article className='text-container'>
           <p>
-            My name is Agustín Arnaiz and I love solving problems.
-            <strong className='distint-color'> My first steps as a programmer were in "C" and "Assembler"</strong> when I was studying
-            Electronics Engineering at the <strong>UTN FRC</strong> back in the year 2000.
-            Today I am a Systems Analyst and Full Stacks Software Developer.
+            My name is Agustín Arnaiz and I am a <strong>Systems Developer</strong>.
+            <strong className='distint-color'> My first steps as a programmer were in "C" and "Assembler"</strong> studying
+            Electronics Engineering at the <strong>UTN FRC</strong> in the year 2000.            
           </p>
           <p>
-            With my work, I have brought significant changes to the company for which I have work, as well as its clients. 
-            <strong className='distint-color'>I have managed to integrate new technologies, and with it, new possibilities. </strong>
+            With my work, I have brought significant changes to the company for which I work, as well as its clients. 
+            <strong className='distint-color'> I managed to integrate new technologies, and with it, new possibilities. </strong>
             How to streamline the purchases processes <strong><i>(GEST2020)</i></strong>  and improve it's after-sales customer service<strong> <i>(TECSEG)</i></strong>.
           </p>    
           <p>
             In addition, the <strong><i>"XME Telemetry"</i></strong>  system <strong className='distint-color'>  
             allowed a new product to be marketed to customers, </strong>
-            By providing the possibility of commanding high power transmitters in remote locations, and with this, being able to provide
-            a better assistance with technical problems.
+            By providing the possibility of commanding high power transmitters in remote locations, and with this, being able <strong className='distint-color'>to provide
+            a better assistance with technical problems.</strong>
           </p>
         </article>        
       </section>
--- src/components/StackIcons.jsx
+++ src/components/StackIcons.jsx
@@ -2,36 +2,37 @@ import '../styles/StackIcons.css'
 import StackIcon from 'tech-stack-icons'
 
 function StackIcons() {
+
   return (
     <div className='new-block' id="stackicons">
       <h2 className='title-color'>&#10070; My Stack</h2>
       <section className='stackicons-container'>
         <div className="fila">
-          <h3 className='text-sutil'>Frontend</h3>
-          <StackIcon className='stackicon' name="html5" />
-          <StackIcon className='stackicon' name="css3" />
-          <StackIcon className='stackicon' name="js" />
-          <StackIcon className='stackicon' name="vitejs" />
-          <StackIcon className='stackicon' name="reactjs" />
-          <StackIcon className='stackicon' name="bootstrap5" />
-          <StackIcon className='stackicon' name="jquery" />          
+          <h3 className='text-sutil'>Frontend</h3>          
+          <StackIcon className='HTML' name="html5"/>
+          <StackIcon className='CSS' name="css3" />
+          <StackIcon className='JavaScript' name="js" />
+          <StackIcon className='Vite JS' name="vitejs" />
+          <StackIcon className='React JS' name="reactjs" />
+          <StackIcon className='Bootstrap' name="bootstrap5" />
+          <StackIcon className='JQuery' name="jquery" />          
         </div>
         <div className="fila">
           <h3 className='text-sutil'>Backend</h3>
-          <StackIcon className='stackicon' name="python" title="Python" />
-          <StackIcon className='stackicon' name="mysql" />
-          <StackIcon className='stackicon' name="django" />
-          <StackIcon className='stackicon' name="flask" />
-          <StackIcon className='stackicon' name="json" />          
-          <StackIcon className='stackicon' name="swagger" />
+          <StackIcon className='Python' name="python" />
+          <StackIcon className='MySQL' name="mysql" />
+          <StackIcon className='Django' name="django" />
+          <StackIcon className='Flask' name="flask"/>
+          <StackIcon className='JSON' name="json"/>          
+          <StackIcon className='Swagger' name="swagger"  />
         </div>
         <div className="fila">  
           <h3 className='text-sutil'>Tools & Agile</h3>                
-          <StackIcon className='stackicon' name="docker" />
-          <StackIcon className='stackicon' name="git" />
-          <StackIcon className='stackicon' name="github" />
-          <StackIcon className='stackicon' name="vscode" />
-          <StackIcon className='stackicon' name="jira" />
+          <StackIcon className='Docker' name="docker" />
+          <StackIcon className='Git' name="git" />
+          <StackIcon className='GitHub' name="github"/>
+          <StackIcon className='VSCode' name="vscode" />
+          <StackIcon className='Jira' name="jira" />
         </div>
       </section>
     </div>
