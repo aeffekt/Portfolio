626d16e24a9dd7ffe53a24960602c4cc65a9fdce
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-08-12 12:20:28 -0300

    animation to social icons & to distint-color class

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -1,24 +1,25 @@
 import logoGit from '../assets/github.svg'
 import logoLinkedIn from '../assets/linkedin.svg'
 import logoCv from '../assets/cv.svg'
+import '../styles/Redes.css'
 
 function Redes() {
   return (
     <div id="redes" className='redes-container'>
       <a href="https://www.linkedin.com/in/arnaizagustin/" target="_blank">
-            <i title='Visit LinkedIn'>
-              <img className='icon' src={logoLinkedIn} alt="Logo LinkedIn"></img>
-            </i></a>
-          <a href="https://github.com/aeffekt" target="_blank">
-            <i title='Visit GitHUB'>
-              <img className='icon' src={logoGit} alt="Logo GitHub" />
-            </i></a>
-          <a href="https://docs.google.com/document/d/1_1Px_wxdpUMe5sOGYl7K1nqZMcMF8k0uviIqJG003RA/edit?usp=sharing"
-            target="_blank">
-            <i title='Open CV file'>
-              <img className='icon' src={logoCv} alt="Logo CV" />
-            </i>
-          </a>          
+        <i title='Visit LinkedIn'>
+          <img className='icon' src={logoLinkedIn} alt="Logo LinkedIn"></img>
+        </i></a>
+      <a href="https://github.com/aeffekt" target="_blank">
+        <i title='Visit GitHUB'>
+          <img className='icon' src={logoGit} alt="Logo GitHub" />
+        </i></a>
+      <a href="https://docs.google.com/document/d/1_1Px_wxdpUMe5sOGYl7K1nqZMcMF8k0uviIqJG003RA/edit?usp=sharing"
+        target="_blank">
+        <i title='Open CV file'>
+          <img className='icon' src={logoCv} alt="Logo CV" />
+        </i>
+      </a>
     </div>
   )
 }
