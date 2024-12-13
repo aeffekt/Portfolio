f3c9d197a6ca42ee4a7511b2577fd122495b59f9
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-12-13 11:55:43 -0300

    no more emailjs nor recaptcha

--- README.md
+++ README.md
@@ -8,16 +8,5 @@ create .env file with your 3 keys for emailjs: VITE_SERVICE_ID, VITE_TEMPLATE_ID
 run: > npm run dev
 
 packages:
-@mailjs/browser (contact email)
-react-google-recaptcha
 sonner (toast)
 tech-stack-icons (reemplazado por svg in JSON file)
-npm i @vercel/speed-insights
-
-Para usar emailjs:
-1- crear cuenta,
-2- crear template,
-3- registrarse en google reCaptha,
-4- registrar dominio (+localhost), usar V2.
-5- import ReCaptcha y setear: sitekey="YOUR_WEB_KEY".
-6- En settings del template, reCaptcha_secret_key="YOUR_SECRET_KEY"
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,70 +1,20 @@
-import React, { useRef } from 'react'
-import emailjs from '@emailjs/browser'
-import ReCAPTCHA from "react-google-recaptcha";
 import '../styles/Contacto.css'
 
-import { Toaster, toast } from 'sonner'
-
-
-export const Contacto = () => {  
-  // Variables de entorno
-  const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
-  const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID 
-  const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY 
-  const RECAPTCHA_WEB_SITE = import.meta.env.VITE_RECAPTCHA_WEB_SITE    
-
-  const form = useRef();
-  function sendEmail(e) {
-    e.preventDefault();
-
-    // Verificar si el valor del captcha es nulo
-    const captchaValue = form.current['g-recaptcha-response'].value;
-    if (!captchaValue) {
-      toast.warning("Please complete the reCAPTCHA.");
-      return;
-    }
-    
-
-    toast.info("Sending...") 
-    emailjs
-      .sendForm(`${SERVICE_ID}`, `${TEMPLATE_ID}`, form.current, {
-        publicKey: `${PUBLIC_KEY}`,
-      })
-      .then(
-        () => {
-          toast.success('Message sent!')          
-        },
-        (error) => {          
-          toast.error("Failed to send the message, please try again!")
-          console.log(error.text);
-        },
-      );
-  };
-
+const Contacto = () => {
+  
   return (
-    <div className='new-block'>      
-      <h2 className='title-color'>&#9998;  Contact me</h2>
-      <section className='contacto-container'>              
-        <form ref={form} onSubmit={sendEmail}>
-          <label htmlFor="user_name">Name</label>
-          <input type="text" name="user_name" id='user_name' />
-          <label htmlFor="user_email">Email</label>
-          <input type="email" name="user_email" id='user_email' required />
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id='message' required />           
-          <input type="submit" value="Send" />
-          
-          <ReCAPTCHA
-            className='recaptcha'            
-            name="captcha"
-            sitekey={RECAPTCHA_WEB_SITE}            
-            required
-          />
-          
-        </form>
-      </section>
-    </div>    
-  )
-}
+    <div className='new-block'>
+        <h2 className='title-color'>&#9998; Contact me</h2>
+        <section className='contacto-container'>            
+            <div>
+                <small>Mail:</small><i className='distint-color'> arnaizagustin@gmail.com</i>                
+                <br />
+                <small>Phone:</small><i className='distint-color'> +54-351-3281199</i>
+            </div>
+            
+        </section>
+    </div>
+  );
+};
 
-export default Contacto
+export default Contacto;
